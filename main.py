a00c22c9ca895eec944f889ae87a6616d964f112
Anderson Peruci <anderson.peruci@hotmail.com>
2022-12-23 02:01:01 -0300

    Versions that works

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+from pytube import YouTube
+import os
+
+music_list = []
+
+for music in music_list:
+    print(f'Starting download: {music}')
+
+    yt = YouTube(music)
+
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path="./musics")
+
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+
+    print('Download completed\n')
+
 # from __future__ import unicode_literals
 # import youtube_dl
 #
@@ -17,33 +36,3 @@
 #     ydl.download([link])
 #
 # print("Contact me on discord for any problem: https://discord.gg/BgjwgWx")
-
-from pytube import YouTube
-import os
-
-music_list = [
-    'https://www.youtube.com/watch?v=oYi_2oz9LAw',
-    'https://www.youtube.com/watch?v=URE0riL21Dc',
-    'https://www.youtube.com/watch?v=hlg4Zmt_I-Y',
-    'https://www.youtube.com/watch?v=7djMIjhZOro',
-    'https://www.youtube.com/watch?v=xcNn7Du1Ok4',
-    'https://www.youtube.com/watch?v=dc6oADkbQSw',
-    'https://www.youtube.com/watch?v=FE7Q950VptY',
-    'https://www.youtube.com/watch?v=ROMqfy1uZbQ',
-    'https://www.youtube.com/watch?v=jGd3V8lRqA4',
-    'https://www.youtube.com/watch?v=xxBIdj3MX9U'
-]
-
-for music in music_list:
-    print(f'Starting download: {music}')
-
-    yt = YouTube(music)
-
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path="./musics")
-
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-
-    print('Download completed\n')
